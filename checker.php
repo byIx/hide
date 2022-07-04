9234eccfa2b251f43a2f81a9fe42417fa3f644b5
byIx <40878600+byIx@users.noreply.github.com>
2022-07-04 22:36:22 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.3.3", "longversion":13300, "released":1656783003, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2679-k4-hidden-content\/" }
+{ "version":"2.0.1", "longversion":20100, "released":1656952500, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2679-k4-hidden-content\/" }
